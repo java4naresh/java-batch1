bd3eaaa1fca049a42a0c47c3a6a19aef0ffb97b2
java4naresh <kambalanaresh4java@gmail.com>
2020-11-17 07:12:29 +0530

    I have written conditional statements example.

---
+++ Jhansi/Java-Batch1/src/com/naresh/javabatch1/ConditionalStatements.java
@@ -0,0 +1,20 @@
+package com.naresh.javabatch1;
+
+public class ConditionalStatements {
+
+	public static void main(String[] args) {
+		// Conditional Statements Examples
+		int num = 1;
+		if(num == 1) {
+			System.out.print("Number is 1");
+		} else if(num == 2) {
+			System.out.print("Number is 2");
+		} else if(num == 3) {
+			System.out.print("Number is 3");
+		} else {
+			System.out.print("Number is not 1,2,3");
+		}
+
+	}
+
+}
