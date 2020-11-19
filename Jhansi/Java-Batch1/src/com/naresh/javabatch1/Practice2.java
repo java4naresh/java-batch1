e6bc2d4adb08d30ef1be94eade1bca40298b0934
Jhansi2425 <jhansi1824@gmail.com>
2020-11-19 07:05:32 +0530

    I have completed for loop task

--- Jhansi/Java-Batch1/src/com/naresh/javabatch1/Practice2.java
+++ Jhansi/Java-Batch1/src/com/naresh/javabatch1/Practice2.java
@@ -6,7 +6,7 @@ public class Practice2 {
 		// TODO Auto-generated method stub
 
 		for(int i= 1;i<=5;i++) {
-			for(int j= 1;j<=6;j++) {
+			for(int j= 1;j<=6;j++) { 
 				System.out.print(j);
 			}
 			System.out.println("");
