8344f71476405f142ef44651dffa1e1f30c247b2
Jhansi2425 <jhansi1824@gmail.com>
2020-11-19 07:08:10 +0530

    first task

--- Jhansi/Java-Batch1/src/com/naresh/javabatch1/Practice1.java
+++ Jhansi/Java-Batch1/src/com/naresh/javabatch1/Practice1.java
@@ -1,23 +1,19 @@
 package com.naresh.javabatch1;
 
 public class Practice1 {
-	public static void main(String[] args) 
-	{
-		
+	public static void main(String[] args) {
+
 		int Unit = 12;
-			if(Unit<=10){
-			System.out. println(Unit*10);
+		if (Unit <= 10) {
+			System.out.println(Unit * 10);
+		} else if (Unit > 10 && Unit <= 15) {
+			System.out.println(Unit * 15);
+		} else if (Unit > 15 && Unit <= 20) {
+			System.out.println(Unit * 20);
+		} else {
+
+			System.out.println(Unit * 30);
 		}
-			else if(Unit >10 && Unit<=15){
-				System.out. println(Unit*15);
-		}
-			else if(Unit>15 && Unit<=20){
-				System.out. println(Unit*20);
-			}
-			else{
-			
-				System.out. println(Unit*30);
-			}
 
 	}
 
