8a81ee5558459d572a06c07420a22d302af0f97a
PonnagantiTriveni <74577558+PonnagantiTriveni@users.noreply.github.com>
2020-11-20 12:12:29 +0200

    Add New Java Project

---
+++ Triveni/Training/src/com/triveni/Training/Loopingstatements.java
@@ -0,0 +1,18 @@
+package com.triveni.Training;
+
+public class Loopingstatements 
+{
+	public static void main(String[] args)
+	{
+		for (int i=1;i<=6;i++)
+		{
+		
+			for(int j=1;j<=6;j++)
+			{			
+				System.out.print(j);
+			}
+		System.out.println();
+		}
+	}
+
+}
