c04f58a86382c26dc49c57bfa59eae607a91420c
saikumar <thalla.saikumar25@gmail.com>
2020-11-25 07:41:07 +0530

    Example if encapsulation

---
+++ Sai/corejavaprograms/src/com/sai/test/AccTest.java
@@ -0,0 +1,20 @@
+package com.sai.test;
+
+import corejavaprograms.Account;
+
+public class AccTest {
+
+	public static void main(String[] args) {
+        Account ac=new Account();
+             ac.setAcc_no(104537338L);
+             ac.setName("sai");
+             ac.setAmount(10000f);
+             System.out.println(ac.getAcc_no());
+             System.out.println(ac.getName());
+             System.out.println(ac.getAmount());
+             
+             
+              
+	}
+
+}
