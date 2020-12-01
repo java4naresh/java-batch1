f528a8e7e641fcc49364fd6e778f142359038177
DivyaReddyMuskula <74784126+DivyaReddyMuskula@users.noreply.github.com>
2020-12-01 12:28:22 +0530

    .

--- Divya/corejavaprograms/src/demo/SingleLevelInherit.java
+++ Divya/corejavaprograms/src/demo/SingleLevelInherit.java
@@ -22,7 +22,7 @@ class B extends A
  {
 	 public static void main(String[] args)
 	 {
-		System.out.println("=========");
+		System.out.println("===========");
 		A a1=new A(5);
 		System.out.println(a1.i);
 		B b1=new B(7,8);
