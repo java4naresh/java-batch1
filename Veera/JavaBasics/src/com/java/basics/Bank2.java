fc72866a31e654bc1cdd4c04eaa5d186817b452f
Veerababu <Veerababu5java@gmail.com>
2020-12-01 02:11:34 -0500

    addedd a+B

--- Veera/JavaBasics/src/com/java/basics/Bank2.java
+++ Veera/JavaBasics/src/com/java/basics/Bank2.java
@@ -4,8 +4,11 @@ public class Bank2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stubS
+		int a=3;
+		int b=5;
 System.out.println("welcome to java program ");
 System.out.println("welcome to india");
+System.out.println(a+b);
 	}
 
 }
