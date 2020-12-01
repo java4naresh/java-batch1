f465f420436d69ab0c005093f96d6f4098299e70
Veerababu <Veerababu5java@gmail.com>
2020-12-01 01:50:56 -0500

    added 2nd program

--- Veera/JavaBasics/src/com/java/basics/Bank2.java
+++ Veera/JavaBasics/src/com/java/basics/Bank2.java
@@ -3,7 +3,7 @@ package com.java.basics;
 public class Bank2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stubS
 System.out.println("welcome to java program");
 	}
 
