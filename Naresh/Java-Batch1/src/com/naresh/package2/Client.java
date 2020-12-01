8d2b029165f5fae97871074a6706835adef273d4
java4naresh <kambalanaresh4java@gmail.com>
2020-12-01 07:44:07 +0530

    1/12/2020

--- Naresh/Java-Batch1/src/com/naresh/package2/Client.java
+++ Naresh/Java-Batch1/src/com/naresh/package2/Client.java
@@ -1,7 +1,9 @@
 package com.naresh.package2;
 
 import com.naresh.javabatch1.Arithmetic;
-import com.naresh.javabatch1.EmailRegistration;
+import com.naresh.javabatch1.Oracle;
+import com.naresh.javabatch1.SoftwareIndustry;
+import com.naresh.javabatch1.TCS;
 
 public class Client extends Arithmetic{
 	
@@ -13,7 +15,7 @@ public class Client extends Arithmetic{
 	public static void main(String[] args) {
 		//Client arith = new Client();
 		//arith.addition(10, 20);
-		EmailRegistration eR = new EmailRegistration();
+		//EmailRegistration eR = new EmailRegistration();
 		/*System.out.println(eR.firstName);
 		System.out.println(eR.lastName);
 		System.out.println(eR.gender);
@@ -27,13 +29,26 @@ public class Client extends Arithmetic{
 		System.out.println(eR.gender);
 		System.out.println(eR.phoneNo);
 		System.out.println(eR.address);*/
-		eR.setFirstName("Naresh");
+		/*eR.setFirstName("Naresh");
 		eR.setLastName("Kambala");
 		eR.setEmail("KambalaNaresh4java");
 		System.out.println(eR.getFirstName());
 		System.out.println(eR.getLastName());
-		System.out.println(eR.getEmail());
-
+		System.out.println(eR.getEmail());*/
+		//26/11/2020
+		/*BANK bank = new SBI();
+		System.out.println(bank.interestRateCalculation());
+		BANK hdfc = new HDFC();
+		System.out.println(hdfc.interestRateCalculation());
+		BANK axis = new AXIS();
+		System.out.println(axis.interestRateCalculation());
+		BANK myOwn = new MyOwn();
+		System.out.println(myOwn.interestRateCalculation());*/
+		//1/12/2020
+		SoftwareIndustry si = new TCS();
+		System.out.println(si.calculateSalary("Developer", 5));
+		SoftwareIndustry oracle = new Oracle();
+		System.out.println(oracle.calculateSalary("Developer", 5));
 	}
 
 }
