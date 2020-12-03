2a6221d8c8a63136e529ec6d255020dd6cf2a87f
java4naresh <kambalanaresh4java@gmail.com>
2020-12-03 07:47:14 +0530

    2/12/2020

--- Naresh/Java-Batch1/src/com/naresh/package2/Client2.java
+++ Naresh/Java-Batch1/src/com/naresh/package2/Client2.java
@@ -1,20 +1,37 @@
 package com.naresh.package2;
 
+import com.naresh.javabatch1.Citizens;
 import com.naresh.javabatch1.EmailRegistration;
 
 public class Client2 {
+	
+	
+	private static int num = getNum();
+	
+	public static int getNum() {
+		System.out.println("getNum");
+		return 10;
+	}
+	
 
 	public static void main(String[] args) {
 		//EmailRegistration er = new EmailRegistration("Naresh", "Kambala", "Male", "9951288766", "Hyd", "kambalanaresh4java@gmail.com");
-		EmailRegistration er2 = new EmailRegistration();
+		/*EmailRegistration er2 = new EmailRegistration();
 		EmailRegistration er = new EmailRegistration("Naresh", "Kambala", "Male");
 		System.out.println(er.getFirstName());
 		System.out.println(er.getLastName());
 		System.out.println(er.getEmail());
 		System.out.println(er.getAddress());
 		System.out.println(er.getGender());
-		System.out.println(er.getPhoneNo());
+		System.out.println(er.getPhoneNo());*/
+		//Citizens v = new Citizens("Veera", "123456", "8341631639", "yusufguda", "Bhagyanagar");
+		//Citizens v2 = new Citizens("Naresh", "123456", "8341631639", "yusufguda", "Bhagyanagar");
+		//System.out.println(v);
+		//System.out.println(v2);
+		System.out.println("Main");
 
 	}
+	
+	
 
 }
