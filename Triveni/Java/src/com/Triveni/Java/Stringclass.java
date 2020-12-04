9e0ba3cba5fc86951d328cd93f66b4876e09a888
PonnagantiTriveni <74577558+PonnagantiTriveni@users.noreply.github.com>
2020-12-04 11:16:35 +0200

    Added StringClass code 4/12/2020

---
+++ Triveni/Java/src/com/Triveni/Java/Stringclass.java
@@ -0,0 +1,31 @@
+package com.Triveni.Java;
+
+public class Stringclass {
+	String name;
+
+	Stringclass(String name) {
+		this.name = name;
+
+	}
+
+	public static void main(String[] args) {
+		int i, j;
+		Stringclass s1 = new Stringclass("Ponnaganti Triveni");
+		System.out.println(s1.name);
+		/*
+		 * for( i=s1.name.length()-1;i>=0;i--) { System.out.print(s1.name.charAt(i));
+		 * 
+		 * }
+		 */
+
+		String[] s = s1.name.split(" ");
+		for (int j1 = 0; j1 < s.length; j1++) {
+			String a = s[j1];
+			StringBuffer sb = new StringBuffer(a);
+			System.out.print(sb.reverse());
+
+		}
+
+	}
+
+}
