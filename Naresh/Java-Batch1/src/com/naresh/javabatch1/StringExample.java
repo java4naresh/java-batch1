38be182370f279ce68a81a7ad5a1b95844880979
java4naresh <kambalanaresh4java@gmail.com>
2020-12-07 21:29:06 +0530

    5/12/2020

--- Naresh/Java-Batch1/src/com/naresh/javabatch1/StringExample.java
+++ Naresh/Java-Batch1/src/com/naresh/javabatch1/StringExample.java
@@ -1,5 +1,7 @@
 package com.naresh.javabatch1;
 
+import java.util.Arrays;
+
 public class StringExample {
 	
 	int num;
@@ -31,7 +33,18 @@ public class StringExample {
 	public void setS(String s) {
 		this.s = s;
 	}
+	
+	
 
+	/*@Override
+	public int hashCode() {
+		return 1;
+	}*/
+
+	@Override
+	public String toString() {
+		return "StringExample [num=" + num + ", fullName=" + fullName + ", s=" + s + "]";
+	}
 
 	public static void main(String[] args) {
 		StringExample se = new StringExample();
@@ -50,7 +63,39 @@ public class StringExample {
 		}*/
 		System.out.print(" ");
 		}
+		System.out.println();
+		System.out.println(se.hashCode());
+		System.out.println(se.toString());
+		String course3 = new String("Java");
+		String course = "Java";
+		String course2 = "Java";
+		
+		System.out.println(course == course2);
+		System.out.println(course == course3);
+		System.out.println(course.equals(course2));
+		System.out.println(course.equals(course3));
+		String courses = "Java Angular Mysql";
+		System.out.println(courses);
+		System.out.println(courses.substring(0, 4));
+		System.out.println(courses.substring(0, 3));
+		String []courses1 = {"Java", "Angular", "Mysql", "AWS"};
+		for (String string : courses1) {
+			System.out.println(string);
+		}
+		System.out.println(Arrays.toString(courses1));
+		for (int i = 0; i < courses1.length; i++) {
+			for (int j = i; j < courses1.length; j++) {
+				System.out.println(courses1[i].compareTo(courses1[j]));
+				if(courses1[i].compareTo(courses1[j]) < 0) {
+					String temp = courses1[i];
+					courses1[i] = courses1[j];
+					courses1[j] = temp;
+				}
+			}
+		}
+		
 
+		System.out.println(Arrays.toString(courses1));
 	}
 
 }
