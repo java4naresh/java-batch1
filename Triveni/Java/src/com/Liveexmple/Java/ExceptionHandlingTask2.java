b569b72d28f085d69f67c7e92b97db309eaa304f
PonnagantiTriveni <74577558+PonnagantiTriveni@users.noreply.github.com>
2020-12-27 16:35:07 +0200

    With out using throws keyword in Exception Handling

--- Triveni/Java/src/com/Liveexmple/Java/ExceptionHandlingTask2.java
+++ Triveni/Java/src/com/Liveexmple/Java/ExceptionHandlingTask2.java
@@ -67,7 +67,7 @@ public class ExceptionHandlingTask2 {
 		}
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		// Arthematic operations
 		int num2 = 100;
 		int num3 = 10;
@@ -81,6 +81,7 @@ public class ExceptionHandlingTask2 {
 			System.out.println(num2 - num3);
 		if (num2 != 0 & num3 != 0)
 			System.out.println(num2 % num3);
+
 		try {
 			readFirstFile1();
 			readSecondFile1();
@@ -89,8 +90,7 @@ public class ExceptionHandlingTask2 {
 			readFifthFile1();
 
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.err.println("Exception");
 		}
 	}
-
 }
