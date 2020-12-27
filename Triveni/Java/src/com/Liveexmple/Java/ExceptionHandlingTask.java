794e87c954aa1ef7b411252a761fba94d761217b
PonnagantiTriveni <74577558+PonnagantiTriveni@users.noreply.github.com>
2020-12-27 16:04:01 +0200

    Added Exception Handling code 27/12/2020

--- Triveni/Java/src/com/Liveexmple/Java/ExceptionHandlingTask.java
+++ Triveni/Java/src/com/Liveexmple/Java/ExceptionHandlingTask.java
@@ -14,11 +14,44 @@ public class ExceptionHandlingTask {
 
 		fileReader = new FileReader(new File(""));
 		System.out.println(fileReader.read());
-
 	}
 
-	public static void main(String[] args) {
+	private static void readSecondFile() throws IOException {
+		FileReader fileReader;
 
+		fileReader = new FileReader(new File(""));
+		System.out.println(fileReader.read());
+	}
+
+	private static void readThirdFile() throws IOException {
+		FileReader fileReader;
+
+		fileReader = new FileReader(new File("Third"));
+		System.out.println(fileReader.read());
+	}
+
+	private static void readFourthFile() throws IOException {
+		FileReader fileReader;
+
+		fileReader = new FileReader(new File(""));
+		System.out.println(fileReader.read());
+	}
+
+	private static void readFifthFile() throws IOException {
+		FileReader fileReader;
+
+		fileReader = new FileReader(new File(""));
+		System.out.println(fileReader.read());
+
+	
+}
+	public static void main(String[] args) {
+		String file1="First";
+		String file2="Second";
+		String file3="Third";
+		String file4="Fourth";
+		String file5="Fifth";
+		
 		String name = "Triveni";
 		int num1 = 100;
 		int num2 = 10;
@@ -27,16 +60,22 @@ public class ExceptionHandlingTask {
 		if (num1 != 0 && num2 != 0)
 			System.out.println(num1 / num2);
 		try {
-			readFile();
+			if (file1.equals("First"))
+				System.out.println("First file is Printed");
 
+			if(file2.equals("Second"))
+				System.out.println("Second file is Printed");
+			if(file3.equals("Third"))
+				System.out.println("Third file is Printed");
+			if(file4.equals("Fourth"))
+				System.out.println("Fourth file is Printed");
+			if(file5.equals("Fifth"))
+				System.out.println("Fifth file is Printed");
+			 
+			 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-	}
-
-	private static void readFile() {
-		// TODO Auto-generated method stub
 
 	}
 
