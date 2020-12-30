858f8fc500637f0fc62026833a5d6a3b41a53b96
java4naresh <kambalanaresh4java@gmail.com>
2020-12-30 07:24:23 +0530

    30/12/2020

---
+++ Naresh/Java-Batch1/src/com/naresh/javabatch1/SingleThread.java
@@ -0,0 +1,18 @@
+package com.naresh.javabatch1;
+
+public class SingleThread {
+
+	public static void main(String[] args) {
+		//System.out.println(Thread.currentThread().getName());
+		System.out.println("Hello World");
+		System.out.println("Welcome to Single Thread");
+		System.out.println("hjggjjh");
+		int num = 10;
+		if(num == 10) {
+			System.out.println("Number is ten");
+		} else {
+			System.out.println("Number is not ten");
+		}
+	}
+
+}
