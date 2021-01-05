1213c15b7094802cc894a8374fbd8cd1b068277e
java4naresh <kambalanaresh4java@gmail.com>
2021-01-05 07:22:37 +0530

    5/1/2021

---
+++ Naresh/Java-Batch1/src/com/naresh/javabatch1/ArrayLimitations.java
@@ -0,0 +1,16 @@
+package com.naresh.javabatch1;
+
+import java.util.ArrayList;
+
+public class ArrayLimitations {
+
+	public static void main(String[] args) {
+		int[] arr = {1,2,3,4,5};
+		int[] arr2 = new int[5];
+		arr[5] = 8;
+		
+		
+		
+	}
+
+}
---
+++ Naresh/Java-Batch1/src/com/naresh/javabatch1/ArrayListExample.java
@@ -0,0 +1,36 @@
+package com.naresh.javabatch1;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ArrayListExample {
+
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		System.out.println(al.size());
+		al.add(1);
+		al.add(1);
+		al.add("Naresh");
+		al.add("Naresh");
+		System.out.println(al.size());
+		System.out.println(al);
+		System.out.println(al.get(0));
+		System.out.println(al.get(3));
+		al.add(2, 6);
+		System.out.println(al.size());
+		System.out.println(al);
+		al.remove(2);
+		System.out.println(al.size());
+		System.out.println(al);
+		LinkedList ll = new LinkedList();
+		ll.add(1);
+		ll.add(1);
+		ll.add("Naresh");
+		ll.add("Naresh");
+		System.out.println(ll.size());
+		System.out.println(ll);
+		System.out.println(ll.get(0));
+		System.out.println(ll.get(3));
+	}
+
+}
