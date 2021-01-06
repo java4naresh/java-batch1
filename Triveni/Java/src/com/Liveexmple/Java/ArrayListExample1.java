acfb12be88c5b60591c122e7dee9d46206f9cc33
PonnagantiTriveni <74577558+PonnagantiTriveni@users.noreply.github.com>
2021-01-06 18:27:53 +0200

    Added ArrayList with Generics code 6/1/2021

---
+++ Triveni/Java/src/com/Liveexmple/Java/ArrayListExample1.java
@@ -0,0 +1,28 @@
+package com.Liveexmple.Java;
+
+import java.util.ArrayList;
+
+public class ArrayListExample1 {
+	private static final int Integer = 0;
+
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		al.add("Triveni");
+		al.add("Durga");
+		al.add("Sai");
+		al.add(1);
+		al.add(2);
+		al.add(3);
+		System.out.println(al);
+
+		int[] intarr = new int[2];
+		String[] strarr = new String[2];
+		for (int i = 0; i < al.size(); i++)
+			if (al.get(i) instanceof Integer)
+				intarr[0] = (Integer) al.get(i);
+			else
+				strarr = (String[]) al.get(i);// here class cast Exception to overcome this exception using Generics
+		// class cast means to change integer to string this is class cast Exception.
+	}
+
+}
---
+++ Triveni/Java/src/com/Liveexmple/Java/ArrayListExample2.java
@@ -0,0 +1,41 @@
+package com.Liveexmple.Java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample2 {
+
+	public static void main(String[] args) {
+		ArrayList<String> a1 = new ArrayList();
+		ArrayList<String> a2 = new ArrayList();
+	
+		a1.add("Triveni");//0
+		a1.add("Durga");//1
+		a1.add("sai");//2
+		a1.add(1, "ABC");//3
+		a1.add(2, "CDE");//4
+		a1.add(3, "FGH");//5
+		System.out.println(a1);
+		a1.set(0, "Mouni");
+		System.out.println(a1);
+		a1.add("Kanna");
+		System.out.println(a1);
+		// collections.sort(a1);
+		System.out.println(a1.get(1));
+		System.out.println(" 1 " + a1.clone());
+		System.out.println(" 2 " + a1.contains("Sai"));
+		System.out.println(" 3 " + a1.equals(2));
+		a1.ensureCapacity(1);
+		System.out.println(" 4 " + a1.hashCode());
+		System.out.println(" 5 " + a1.indexOf(2));
+		System.out.println(" 6 " + a1.isEmpty());
+		System.out.println(" 7 " + a1.lastIndexOf("Durga"));
+		System.out.println(" 8 " + a1.lastIndexOf(2));
+		System.out.println(" 9 " + a2.addAll(a1));
+		System.out.println(a1.retainAll(a2));
+		System.out.println(a1);
+		
+
+	}
+}
+	
