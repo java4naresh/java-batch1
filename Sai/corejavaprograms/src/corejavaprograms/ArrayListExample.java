e5a2105dac111e8ce0e9d06b6af7de7a9f11b750
saikumar <thalla.saikumar25@gmail.com>
2021-01-11 22:17:21 +0530

    ArrayListExe

--- Sai/corejavaprograms/src/corejavaprograms/ArrayListExample.java
+++ Sai/corejavaprograms/src/corejavaprograms/ArrayListExample.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 
 public class ArrayListExample {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		ArrayList al=new ArrayList();
 		al.add("S");
