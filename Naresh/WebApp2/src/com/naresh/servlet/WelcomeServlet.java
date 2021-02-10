4be075e86ad8ed2ea04bfe5024e66a20ac212d50
java4naresh <kambalanaresh4java@gmail.com>
2021-02-10 07:14:41 +0530

    10/2/2021 first servlet

---
+++ Naresh/WebApp2/src/com/naresh/servlet/WelcomeServlet.java
@@ -0,0 +1,44 @@
+package com.naresh.servlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class WelcomeServlet implements Servlet {
+
+	@Override
+	public void destroy() {
+      System.out.println("destroy");
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		return null;
+	}
+
+	@Override
+	public String getServletInfo() {
+		return null;
+	}
+
+	@Override
+	public void init(ServletConfig arg0) throws ServletException {
+		System.out.println("init");
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		        System.out.println("service");
+                PrintWriter pw = res.getWriter();
+                pw.write("<html>");
+        		pw.write("<body>");
+        		pw.write("<p1>Welcome to My Server</p1>");
+        		pw.write("</body>");
+        		pw.write("</html>");
+	}
+
+}
