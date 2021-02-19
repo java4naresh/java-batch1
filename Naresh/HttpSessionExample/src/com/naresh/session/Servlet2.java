31de1da9814c9873e332e4e413e31671f114074b
java4naresh <kambalanaresh4java@gmail.com>
2021-02-19 07:14:11 +0530

    19/2/2021 session examples

---
+++ Naresh/HttpSessionExample/src/com/naresh/session/Servlet2.java
@@ -0,0 +1,34 @@
+package com.naresh.session;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Servlet2
+ */
+@WebServlet("/Servlet2")
+public class Servlet2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(true);
+		response.getWriter().append("Served at: ").append("Your message is:"+session.getValue("msg"));
+	}
+
+}
