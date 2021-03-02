a6f716877e5d32a646d0817377397fc316f41a36
sai <thalla.saikumar25@gmail.com>
2021-03-02 20:01:12 +0530

    MVNExample

---
+++ Sai/First-mvn/src/main/java/com/sai/spring/Owner.java
@@ -0,0 +1,52 @@
+package com.sai.spring;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Owner {
+   
+	   private List<String> companies;
+	   private Set<String>  address;
+	   private Map<Integer,String> employees;
+
+	   public Owner() {
+		   
+	   }
+
+	public Owner(List<String> companies, Set<String> address, Map<Integer, String> employees) {
+		super();
+		this.companies = companies;
+		this.address = address;
+		this.employees = employees;
+	}
+
+	public List<String> getCompanies() {
+		return companies;
+	}
+
+	public void setCompanies(List<String> companies) {
+		this.companies = companies;
+	}
+
+	public Set<String> getAddress() {
+		return address;
+	}
+
+	public void setAddress(Set<String> address) {
+		this.address = address;
+	}
+
+	public Map<Integer, String> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(Map<Integer, String> employees) {
+		this.employees = employees;
+	}
+
+	@Override
+	public String toString() {
+		return "Owner [companies=" + companies + ", address=" + address + ", employees=" + employees + "]";
+	}   
+}
