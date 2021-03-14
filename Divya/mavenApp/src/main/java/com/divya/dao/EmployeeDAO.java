6627727513338f43b9a60d37855015cb3bb2fc6b
DivyaReddyMuskula <74784126+DivyaReddyMuskula@users.noreply.github.com>
2021-03-14 23:29:14 +0530

    Spring JDBC-Insert,Update,Delete

--- Divya/mavenApp/src/main/java/com/divya/dao/EmployeeDAO.java
+++ Divya/mavenApp/src/main/java/com/divya/dao/EmployeeDAO.java
@@ -24,7 +24,31 @@ public class EmployeeDAO
 	public void printAllEmployees()
 	{
 		//System.out.println(template.queryForObject("select empName from employee where empId=39",String.class));
-		System.out.println(template.queryForObject("select sal from employee where empId=39",Integer.class));
+		//System.out.println(template.queryForObject("select sal from employee where empId=39",Integer.class));
+		System.out.println(template.queryForList("select empName from employee",String.class)); //Will print all the employeeNames
+		System.out.println(template.queryForList("select empId from employee",Integer.class));
 	}
-
+	public void printSalary(int empId)
+	{
+		System.out.println(template.queryForInt("select sal from employee where empId="+empId));
+	}
+	public void printName(int empId)
+	{
+		System.out.println(template.queryForObject("select empName from employee where empId="+empId,String.class));
+	}
+	public void insert(int empId,String empName,String designation,int sal)
+	{
+		String query="insert into employee values("+empId+",'"+empName+"',"+"'"+designation+"',"+sal+")";
+		System.out.println(template.update(query));
+	}
+	public void update(int empId,String empName)
+	{
+		String query="update employee set empName="+"'"+empName+"' where empId="+empId;
+		System.out.println(template.update(query));
+	}
+	public void delete(int empId)
+	{
+		String query="delete from employee where empId="+empId;
+		System.out.println(template.update(query));
+	}
 }
